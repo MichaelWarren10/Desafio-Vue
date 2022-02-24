3d8f6895c4318de030bed22f518d68cfd0189725
Michael Gomes <99808134+MichaelWarren10@users.noreply.github.com>
2022-02-24 11:40:05 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <ContextColorProvider id="app">
+    <router-view />
+  </ContextColorProvider>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { ContextColorProvider } from '@warrenbrasil/nebraska-web'
+
+@Component({
+  components: {
+    ContextColorProvider
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style scoped>
+@import '~@warrenbrasil/nebraska-tokens-web/assets/colors.css';
+@import '~@warrenbrasil/nebraska-tokens-web/assets/spacings.css';
+@import '~@warrenbrasil/nebraska-tokens-web/assets/typography.css';
+</style>
